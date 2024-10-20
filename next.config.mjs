0dc27fae7827acf82ac69805f2580763bc6d0e0e
sendo-kakeru <sendokakeru.js@gmail.com>
2024-10-21 08:41:59 +0900

    update vercel.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects() {
+    return [
+      {
+        "source": "/search",
+        "has": [
+          {
+            "type": "query",
+            "key": "pref",
+            "value": "23"
+          }
+        ],
+        "destination": "/search?prefectures=23&limit=10&page=1",
+        "permanent": true
+      },
+    ]
+  },
+};
 
 export default nextConfig;
